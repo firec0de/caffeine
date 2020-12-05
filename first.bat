e46c93fa3663c8fd8ed9a11793a92499b23f5d02
firec0de <67567107+firec0de@users.noreply.github.com>
2020-12-05 23:11:08 +0100

    first.bat
    This file is used for copying the files caffeine.exe, caffeineMeter.exe, starter.vbs to the directory %appdata% and creating a shortcut of the file starter.vbs in the Windows startup folder.

--- first.bat
+++ first.bat
@@ -6,7 +6,6 @@ Copy "caffeineMeter.exe" "%AppData%"
 
 Copy "starter.vbs" "%AppData%"
 
-
 echo [InternetShortcut] >> "%Appdata%\Microsoft\Windows\Start Menu\Programs\Startup\autostart.url"
 echo URL="%Appdata%\starter.vbs" >> "%Appdata%\Microsoft\Windows\Start Menu\Programs\Startup\autostart.url"
 echo IconFile=C:\WINDOWS\system32\SHELL32.dll >> "%Appdata%\Microsoft\Windows\Start Menu\Programs\Startup\autostart.url"
